c2753b25157ccf38dae7addb19f5aca6da9d7f0a
DevilsKingX <118957648+AkhilTrivediX@users.noreply.github.com>
2024-08-19 00:30:12 +0530

    metadata update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,9 +7,9 @@ export default function Navbar(){
         <div className="w-screen p-2 border-b-[1px] border-white border-opacity-20 fixed top-0 backdrop-blur-xl">
             <NavigationMenu>
                 <NavigationMenuItem>
-                    <Link href="/docs" legacyBehavior passHref>
+                    <Link href="/" legacyBehavior passHref>
                         <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                        Documentation
+                         <div className="font-[800] text-xl font-mono">XCRAPE</div>
                         </NavigationMenuLink>
                     </Link>
                 </NavigationMenuItem>
